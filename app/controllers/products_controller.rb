aed9941fc2c3954c39e57e4da819d675c28016a8
mattjkatz <matthewkatz7@gmail.com>
2022-03-08 20:06:05 -0600

    add individual routes and methods for displaying products one, two, and three

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,20 @@ class ProductsController < ApplicationController
     all_products = Product.all
     render json: all_products.as_json
   end
+
+  def product_one
+    product_one = Product.find_by(id: 1)
+    render json: product_one.as_json
+  end
+
+  def product_two
+    product_two = Product.find_by(id: 2)
+    render json: product_two.as_json
+  end
+
+  def product_three
+    product_three = Product.find_by(id: 3)
+    render json: product_three.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "all_products"
+  get "/product_one", controller: "products", action: "product_one"
+  get "/product_two", controller: "products", action: "product_two"
+  get "/product_three", controller: "products", action: "product_three"
 end
