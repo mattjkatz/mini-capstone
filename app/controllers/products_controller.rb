16b46a61ab2dc22d97f8c7055712b2054fe335a4
mattjkatz <matthewkatz7@gmail.com>
2022-03-13 11:35:08 -0500

    add the update route and method as well as the request.http file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  
+
   def index
     all_products = Product.all
     render json: all_products.as_json
@@ -10,6 +10,15 @@ class ProductsController < ApplicationController
     all_products = Product.all
     searched_product = all_products.find_by(id: "#{product_id}".to_i)
     render json: {message: "#{searched_product["name"]} is $#{searched_product["price"]}. #{searched_product["description"]}"}
+  end
+
+  def create
+    product = Product.create(
+      name: params["name"],
+      price: params["price"],
+      description: params["description"]
+    )
+    render json: product.as_json
   end
 
 end
--- products_http.rb
+++ products_http.rb
@@ -1,1 +1,9 @@
 require "http"
+
+puts "ENTER: [A] print all [1]"
+input = gets.chomp
+
+if input.downcase == "a"
+  response = HTTP.get("http://localhost:3000/prodcts")
+  puts response.parse
+end
