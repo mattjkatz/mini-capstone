d1a34b2e618baa60aca1cb6274cb935ed5ab09af
mattjkatz <matthewkatz7@gmail.com>
2022-03-23 19:50:27 -0500

    add has_many categories to product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,9 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :price, :description, :is_discounted?, :tax, :total, :supplier_id, :supplier, :images, :user_id
+
+  belongs_to :supplier
+  has_many :images
+  has_many :categories
 end
+
+
